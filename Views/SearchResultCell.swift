dfca2281e0a2e402e53944c1a23f8c1e7f78ac13
kokosDev <maks.kokos.dev@gmail.com>
2022-06-02 14:28:32 +0300

    Cells and UIStackView Layouts

---
+++ Views/SearchResultCell.swift
@@ -0,0 +1,78 @@
+//
+//  SearchResultCell.swift
+//  AppStoreJSONApis
+//
+//  Created by Maks Kokos on 02.06.2022.
+//
+
+import UIKit
+
+class SearchResultCell: UICollectionViewCell {
+        
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .red
+        iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.layer.cornerRadius = 12
+        return iv
+    }()
+    
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Photos & Videos"
+        return label
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "APP NAME"
+        return label
+    }()
+
+    let ratingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "9.26M"
+        return label
+    }()
+    
+    let getButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("GET", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.backgroundColor = .darkGray
+        button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .yellow
+        
+        let labelStackView = UIStackView(arrangedSubviews: [
+            nameLabel, categoryLabel, ratingLabel
+        ])
+        labelStackView.axis = .vertical
+                
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView, labelStackView, getButton
+        ])
+        stackView.spacing = 12
+        stackView.alignment = .center
+        
+        addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
