99b8fabc704610945d22f740b951286e0fedd69a
kokosDev <maks.kokos.dev@gmail.com>
2022-06-05 12:59:35 +0300

    add icon image and screenshots images with cocoaPods

--- Controllers/AppsSearchController.swift
+++ Controllers/AppsSearchController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
@@ -50,11 +51,7 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SearchResultCell
-        
-        let appResult = appResults[indexPath.item]
-        cell.nameLabel.text = appResult.trackName
-        cell.categoryLabel.text = appResult.primaryGenreName
-        cell.ratingLabel.text = .init(format: "%.1f", appResult.averageUserRating ?? 0)
+        cell.appResult = appResults[indexPath.item]
         return cell
     }
     
